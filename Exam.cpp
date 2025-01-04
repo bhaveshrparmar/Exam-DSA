2254cb209e13e59173133b00ee92f936bd240b86
bhaveshrparmar <bp0767609@gmail.com>
2025-01-04 16:25:32 +0530

    Add files via upload

---
+++ Exam.cpp
@@ -0,0 +1,136 @@
+//  Question  ( 1 )
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+   int start = 41;
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << start + j << " ";
+        }
+        cout << endl;
+    }
+}
+
+
+
+
+//  Question  ( 2 )
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num = 11;
+    for (int i = 1; i < 5; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+}
+
+
+
+
+
+//  Question  ( 3 )
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+for (int i = 5; i >= 1; i--) {
+        for (int j = 0; j < i; j++) {
+            cout << (j % 2) << " ";
+        }
+        cout << endl;
+    }
+    }
+
+
+
+//  Question  ( 4 )
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n = 5; 
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j < n; j++) {
+            cout << " "; 
+        }
+
+        for (int j = n - i + 1; j <= n; j++) {
+            cout << j;
+        }
+
+        for (int j = n - 1; j >= n - i + 1; j--) {
+            cout << j;
+        }
+
+        cout << endl; 
+    }
+}
+
+
+ 
+
+
+//  Question  ( 5 )
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;  
+   
+    for (int i = 1; i <= n; i++) {
+        cout << i;
+    }
+    
+    for (int i = 6 - 1; i >= 1; i--) {
+        cout << i;
+    }
+
+    cout << endl;  
+
+}
+
+
+
+
+//  Question  ( 6 )
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;  
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << j;
+        }
+        cout << endl;
+    }
+
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << j;
+        }
+        cout << endl;
+    }
+}
+
+
